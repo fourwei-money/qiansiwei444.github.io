a7f59b4307f211e007cd15b631985a284b0fad5d
bigzjk <bigzjk@163.com>
2019-06-28 16:28:13 +0800

    登录功能完成了

--- src/main.js
+++ src/main.js
@@ -6,11 +6,33 @@ import router from './router'
 import ElementUI from "element-ui"
 // 2 引入css文件
 import 'element-ui/lib/theme-chalk/index.css'
+
+import "./assets/index.css"
 // 安装elementUI
 Vue.use(ElementUI)
-import "./assets/index.css"
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
+
+// 注册一个导航守卫
+// router.beforeEach((to, from, next) => {
+//  console.log("导航守卫工作了！！")
+ 
+// })
+router.beforeEach((to, from, next) => {
+  // console.log(to,from)
+  if(to.path === "/login"){
+    next()
+    return
+  }
+
+  if(localStorage.getItem("token")){
+    next()
+  }else{
+    router.push("/login")
+  }
+//  console.log("导航守卫工作了！！")
+//  next()
+})
 new Vue({
   router,
   render: h => h(App)
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,66 +1,96 @@
 <template>
   <el-row type="flex" class="row-bg" justify="center" align="middle">
     <el-col :xs="12" :sm="10" :md="8" :lg="6">
-      <el-form ref="form" :model="form" label-width="80px" class="login-form" label-position="top" :rules="loginRules">
-        <el-form-item label="用户名" prop="suername">
-          <el-input v-model="form.suername"></el-input>
+      <el-form
+        ref="formName"
+        :model="form"
+        label-width="80px"
+        class="login-form"
+        label-position="top"
+        :rules="loginRules"
+      >
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="form.username"></el-input>
         </el-form-item>
         <el-form-item label="密码" prop="password">
           <el-input v-model="form.password"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="onSubmit">登录</el-button>
-          <el-button>重置</el-button>
+          <el-button type="primary" @click="submitForm('formName')">登录</el-button>
+          <el-button @click="resetForm('formName')">重置</el-button>
         </el-form-item>
       </el-form>
     </el-col>
   </el-row>
 </template>
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
       form: {
-        suername: "",
+        username: "",
         password: ""
       },
       loginRules: {
-        suername: [
+        username: [
           {
             required: true,
             message: "请输入您的用户名",
             trigger: "blur"
+          },
+          {
+            min: 5,
+            max: 12,
+            message: "用户名必须在 5 到 12 个字符",
+            trigger: "change"
           }
         ],
-        password:[
+        password: [
           {
             required: true,
             message: "请输入您的密码",
             trigger: "blur"
+          },
+          {
+            min: 6,
+            max: 12,
+            message: "密码必须在 6 到 12 个字符",
+            trigger: "change"
           }
         ]
       }
     };
   },
   methods: {
-    onSubmit() {
-      console.log("submit!");
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          axios({
+            url: "http://localhost:8888/api/private/v1/login",
+            method: "post",
+            data: this.form
+          }).then(({ data: { data, meta } }) => {
+            // console.log(data)
+            
+            if (meta.status === 200) {
+              localStorage.setItem("token",data.token)
+              this.$router.push("/home");
+            }
+          });
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
-     submitForm(formName) {
-        this.$refs[formName].validate((valid) => {
-          if (valid) {
-            alert('submit!');
-          } else {
-            console.log('error submit!!');
-            return false;
-          }
-        });
-      },
-      resetForm(formName) {
-        this.$refs[formName].resetFields();
-      }
+    resetForm(formName) {
+      // console.log(formName)
+
+      this.$refs[formName].resetFields();
     }
   }
+};
 </script>
 <style>
 .row-bg {
