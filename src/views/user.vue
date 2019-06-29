6c3ca617333a11458291a2de3799ff3b5c2308e6
bigzjk <bigzjk@163.com>
2019-06-29 23:38:53 +0800

    user

--- src/views/user.vue
+++ src/views/user.vue
@@ -1,5 +1,69 @@
 <template>
   <div>
-    <h1>这是user页面</h1>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-input v-model="input" placeholder="请输入内容" size="mini"></el-input>
+    <el-table :data="userList" stripe style="width: 100%">
+      <el-table-column prop="username" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
+      <el-table-column prop="mobile" label="电话"></el-table-column>
+      <el-table-column label="用户状态">
+        <template v-slot="{row}">
+          <el-switch v-model="row.type" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <el-button type="primary" plain icon="el-icon-edit" size="mini"></el-button>
+        <el-button type="danger" plain icon="el-icon-delete" size="mini"></el-button>
+        <el-button type="success" plain icon="el-icon-check" size="mini">分配角色</el-button>
+      </el-table-column>
+    </el-table>
   </div>
 </template>
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      userList: []
+    };
+  },
+  created() {
+    axios({
+      url: "http://localhost:8888/api/private/v1/users",
+      params: {
+        pagenum: 1,
+        pagesize: 5
+      },
+      headers: {
+        Authorization: localStorage.getItem("token")
+      }
+    }).then(res => {
+      console.log(res);
+
+      let {
+        data: { data, meta }
+      } = res;
+      this.userList = data.users;
+    });
+  }
+};
+</script>
+<style lang="less">
+.el-main {
+  .el-breadcrumb {
+    line-height: 50px;
+    background-color: #D4DAE0;
+    padding-left: 20px;
+    font-size: 16px;
+    .is-link{
+      font-weight: 600; 
+    }
+  }
+}
+</style>
+
